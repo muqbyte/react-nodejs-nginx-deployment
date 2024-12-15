f83c9f662816547ddaa1bb0ab4aa4457d43651c0
muqtadir <tadir.muqtadir@gmail.com>
2024-12-15 19:40:52 +0800

    sixth push

--- createENV.sh
+++ createENV.sh
@@ -2,7 +2,7 @@
 
 # Prompt user for input (port numbers)
 read -p "Enter DB_FRONTEND port number: " DB_FRONTEND
-read -p "Enter DB-BACKEND port number: " DB_BACKEND
+read -p "Enter DB_BACKEND port number: " DB_BACKEND  # Fix the variable name
 
 # Create .env file
 if ! touch .env; then
@@ -12,7 +12,7 @@ fi
 
 # Add the required environment variables to the .env file
 echo "DB_FRONTEND=$DB_FRONTEND" >> .env
-echo "DB-BACKEND=$DB_BACKEND" >> .env
+echo "DB_BACKEND=$DB_BACKEND" >> .env  # Fix the variable name
 
 # Make the .env file only readable by the current user
 if ! chmod 600 .env; then
